9134fe95cf4536d657a013004fa231cb3e9de3c5
Gabriel <gabriel941017@icloud.com>
2023-06-13 00:40:39 -0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta name="theme-color" content="#E6E1DE" />
+
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         {{-- Font Awesome --}}

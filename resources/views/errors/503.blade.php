613e9cc6c4856d82a886eeef18db0b3e4a360506
Gabriel91017 <gabriel941017@icloud.com>
2023-07-28 11:13:11 -0600

    503 added

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,14 @@
+<x-guest-layout>
+    <div class="h-screen">
+        <div class="container h-full w-full flex flex-col justify-center items-center text-platinum">
+            <div class="text-center">
+                <h1 class="font-bold font-saol text-8xl mb-8">503</h1>
+                <p class="font-proximaBold text-goldenrod">Sorry we are under maintenance...</p>
+                <p class="mb-2">We are working so that you can buy comfortably.</p>
+                <div class="flex justify-center items-center">
+                    <x-authentication-card-logo size="h-10" />
+                </div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
